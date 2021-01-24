9044ed101a6e3704771cd4690d4411458c62e0e8
VENKAT SUBRAMANIAN <venkat.subramanian@sap.com>
2021-01-24 11:15:13 +0800

    backup

--- ra.py
+++ ra.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
     l_numbers = [11, 7, 17, 27,19, 23, 12,13, 9,18]
     #l_numbers = [11, 7]
     existing_numbers = pipeline.get_numbers(6000,7000)
-    for i in range(100):
+    for i in range(10):
         rand_number = random.randint(6000,7000)
         if(not (str(rand_number) in existing_numbers) ):
             total_value_of_digits = 0;
             for c in str(rand_number):
                 total_value_of_digits += int(c)
-            if(total_value_of_digits in l_numbers):
-                print(f" the random number {rand_number} and the total value of all digits {total_value_of_digits}");
+            #if(total_value_of_digits in l_numbers):
+            print(f" the random number {rand_number} and the total value of all digits {total_value_of_digits}");
